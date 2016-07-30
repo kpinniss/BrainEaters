86b47f5a011203aedd4957ef49f1a101b0580d1b
kpi092191 <kpinniss@gmail.com>
2016-07-30 14:16:45 -0700

    Add files via upload
    movement added//  issue with backdrop image

--- braineaters.ts
+++ braineaters.ts
@@ -1,4 +1,5 @@
 
+
 window.onload = draw; // Execute draw function when DOM is ready
 function draw()
   {
@@ -9,13 +10,32 @@ function draw()
      ctx.drawImage(backDrop,10,10);
 
       let player = new Image();
+      //position
+      let x =120;
+      let y =120;
+      let key,pos = 0;
       player.src = "Wizard_Male.png";
-      player.onload = function(){
-      ctx.drawImage(player,20,20,50,60);
-  }
+      player.onload = function()
+      {
+        ctx.drawImage(player,x ,y ,50,60);
+      }
+      document.onkeydown = function(e)
+      {
+        pos=1;
+        key=window.event?e.keyCode:e.which;
+      }
+      document.onkeyup = function(e)
+      {
+        pos=0;
+      }
+      setInterval(function()
+      {
+        if(pos==0)return;
+        if(key==37)x-=2;
+        if(key==38)y-=2;
+        if(key==39)x+=2;
+        if(key==40)y+=2;
+        canvas.width = canvas.width;
+        ctx.drawImage(player,x ,y ,50,60);
+      },5);
 }
-
- function movePlayer()
- {
-
- }
