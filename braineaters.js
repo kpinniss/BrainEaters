130f10193000f58e83028e194b492db4a6c70200
kpi092191 <kpinniss@gmail.com>
2016-08-01 09:37:23 -0700

    Add files via upload

--- braineaters.js
+++ braineaters.js
@@ -3,14 +3,26 @@ var ctx;
 var background = document.getElementById("background");
 var wizard = document.getElementById("wizard");
 var zombie = document.getElementById("mob");
+var lifeImg = document.getElementById("heart");
+var door = document.getElementById("door");
 function gameLoop() {
     requestAnimationFrame(gameLoop);
     ctx.drawImage(background, 0, 0, 750, 550);
+    life1.draw();
+    life2.draw();
+    life3.draw();
+    door4.draw();
+    door1.draw();
+    door2.draw();
+    door3.draw();
     player.draw();
     zomb1.draw();
-    moveZomb(zomb1);
+    zomb2.draw();
+    zomb3.draw();
+    zomb4.draw();
+    moveZomb(zomb2, zomb3, zomb4);
     playerCollsionBoarder(player, canvas);
-    playerZombieCol(player, zomb1, canvas);
+    playerZombieCol(player, zomb1, canvas, life1, life2, life3);
 }
 window.onload = function () {
     canvas = document.getElementById('myCanvas');
@@ -20,6 +32,27 @@ window.onload = function () {
     document.addEventListener('keydown', keyboardInput);
     gameLoop();
 };
+var cDoor = (function () {
+    function cDoor(x, y, speed, width, height) {
+        var _this = this;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = width;
+        this.height = height;
+        this.draw = function () {
+            ctx.save();
+            ctx.translate(_this.width, _this.height);
+            ctx.translate(_this.x, _this.y);
+            ctx.drawImage(door, 0, 0, 60, 90);
+            ctx.restore();
+        };
+        x = 0;
+        y = 0;
+        speed = 0;
+    }
+    return cDoor;
+}());
 var cPlayer = (function () {
     function cPlayer(x, y, speed, width, height) {
         var _this = this;
@@ -41,12 +74,12 @@ var cPlayer = (function () {
     }
     return cPlayer;
 }());
-var player = new cPlayer(575, 200, 150, 50, 50);
+var player = new cPlayer(350, 300, 0, 50, 60);
 function playerCollsionBoarder(player, canvas) {
-    if (player.x + player.width > canvas.width) {
+    if (player.x + player.width * 2 > canvas.width) {
         player.x = canvas.width - player.width * 2;
     }
-    if (player.y + player.height > canvas.height) {
+    if (player.y + player.height * 2 > canvas.height) {
         player.y = canvas.height - player.height * 2;
     }
     if (player.x < -50) {
@@ -91,14 +124,83 @@ var cMob = (function () {
     }
     return cMob;
 }());
-var zomb1 = new cMob(200, 45, 0, 50, 50);
-function playerZombieCol(player, zomb1, canvas) {
-    var playerPos = (player.x, player.y);
-    var ZombPos = (zomb1.x, zomb1.y);
-    if (playerPos == ZombPos) {
-        playerPos = player.x - player.width;
-        console.log("You died!");
+var zomb1 = new cMob(400, 400, 0, 60, 60);
+var zomb2 = new cMob(0, 60, 0, 60, 60);
+var zomb3 = new cMob(80, 160, 0, 60, 60);
+var zomb4 = new cMob(200, 350, 0, 60, 60);
+var door1 = new cDoor(650, 111, 0, 0, 0);
+var door2 = new cDoor(650, 70, 0, 0, 0);
+var door3 = new cDoor(650, 369, 0, 0, 0);
+var door4 = new cDoor(650, 400, 0, 0, 0);
+function playerZombieCol(player, zomb1, canvas, life1, life2, life3) {
+    if (player.x == zomb1.x && player.y == zomb1.y) {
+        player.x = zomb1.x - player.width;
+        life1.x = -100;
+        life2.x = -100;
+        life3.x = -100;
+        console.log("You are Dead!");
+    }
+    else if (player.x == zomb2.x && player.y == zomb2.y) {
+        player.x = zomb2.x - player.width;
+        life1.x = -100;
+        life1.x = -100;
+        life1.x = -100;
+    }
+    else if (player.x == zomb3.x && player.y == zomb3.y) {
+        player.x = zomb3.x - player.width;
+        life1.x = -100;
+        life1.x = -100;
+        life1.x = -100;
+    }
+    else if (player.x == zomb4.x && player.y == zomb4.y) {
+        player.x = zomb4.x - player.width;
+        life1.x = -100;
+        life1.x = -100;
+        life1.x = -100;
+    }
+    if (life1.x == -100 && life2.x == -100 && life3.x == -100) {
+        alert("You are Dead!!!");
     }
 }
-function moveZomb(zomb1) {
+var cLife = (function () {
+    function cLife(x, y, speed, width, height) {
+        var _this = this;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = width;
+        this.height = height;
+        this.draw = function () {
+            ctx.save();
+            ctx.translate(_this.width, _this.height);
+            ctx.translate(_this.x, _this.y);
+            ctx.drawImage(lifeImg, 0, 0, 30, 30);
+            ctx.restore();
+        };
+        x = 0;
+        y = 0;
+        speed = 0;
+    }
+    return cLife;
+}());
+var life1 = new cLife(0, -30, 0, 50, 50);
+var life2 = new cLife(30, -30, 0, 50, 50);
+var life3 = new cLife(60, -30, 0, 50, 50);
+function moveZomb(zomb2, zomb3, zomb4) {
+    zomb1.x -= 5;
+    zomb2.x -= 3;
+    zomb3.x -= 5;
+    zomb4.x -= 7;
+    if (zomb1.x < -100) {
+        zomb1.x = 650;
+    }
+    if (zomb2.x < -100) {
+        zomb2.x = 650;
+    }
+    if (zomb3.x < -100) {
+        zomb3.x = 650;
+    }
+    if (zomb4.x < -100) {
+        zomb4.x = 650;
+    }
 }
--- braineaters.ts
+++ braineaters.ts
@@ -8,6 +8,10 @@ let background = <HTMLImageElement>document.getElementById("background");
 let wizard = <HTMLImageElement>document.getElementById("wizard");
 //zombie
 let zombie = <HTMLImageElement>document.getElementById("mob");
+//magicBolt
+let lifeImg = <HTMLImageElement>document.getElementById("heart");
+//door
+let door = <HTMLImageElement>document.getElementById("door");
 
 //main loop to redraw canvas at 60fps
 function gameLoop(): void
@@ -17,15 +21,25 @@ function gameLoop(): void
   // ctx.fillRect(0, 0, 800, 500);
 
   ctx.drawImage(background, 0, 0, 750 ,550);
-
+//draw life sprite
+  life1.draw();
+  life2.draw();
+  life3.draw();
+//draw door sprite
+  door4.draw();
+  door1.draw();
+  door2.draw();
+  door3.draw();
+//draw player and enemy sprite
   player.draw();
   zomb1.draw();
-  // zomb2.draw();
-  // zomb3.draw();
-  // zomb4.draw();
-  moveZomb(zomb1);
+   zomb2.draw();
+   zomb3.draw();
+   zomb4.draw();
+//funtions on load
+  moveZomb(zomb2,zomb3,zomb4);
   playerCollsionBoarder(player,canvas);
-  playerZombieCol(player,zomb1, canvas);
+  playerZombieCol(player,zomb1, canvas, life1, life2, life3);
 
 }
 
@@ -51,6 +65,24 @@ interface iGameChar
    speed: number; //movement in pixs per sec
 }
 
+class cDoor implements iGameChar
+{
+  constructor(public x:number, public y:number, public speed:number, public width:number, public height: number){
+    x = 0;
+    y = 0;
+    speed = 0;
+
+  }
+  public draw = (): void =>
+  {
+    ctx.save();
+    ctx.translate(this.width, this.height);
+    ctx.translate(this.x, this.y);
+    ctx.drawImage(door, 0, 0, 60, 90);
+    ctx.restore();
+  }
+}
+
 
 //player char
 class cPlayer implements iGameChar
@@ -71,16 +103,16 @@ class cPlayer implements iGameChar
   }
 }
 
-let player = new cPlayer(575, 200 , 150, 50, 50);
+let player = new cPlayer(350, 300 , 0, 50, 60);
 
 
  function playerCollsionBoarder(player, canvas)
  {
-   if(player.x + player.width > canvas.width)
+   if(player.x + player.width*2 > canvas.width)
    {
      player.x = canvas.width - player.width*2;
    }
-   if(player.y + player.height > canvas.height)
+   if(player.y + player.height*2 > canvas.height)
    {
      player.y = canvas.height - player.height*2;
    }
@@ -114,6 +146,7 @@ function keyboardInput(event: KeyboardEvent)
    else if (event.keyCode == 40 || event.keyCode == 83 ) {
       player.y += 10;
    }
+
 }
 
 //zombie char
@@ -137,114 +170,120 @@ class cMob implements iGameChar
 }
 //
 
-let zomb1 = new cMob(200, 45, 0, 50, 50)
-//let zomb2 = new cMob(0,0, 0, 50 ,50)
-//let zomb3 = new cMob(80, 54, 100, 0 ,0)
-//let zomb4 = new cMob(200, 111, 100, 0, 0)
+let zomb1 = new cMob(400, 400, 0, 60, 60)
+let zomb2 = new cMob(0,60, 0, 60, 60)
+let zomb3 = new cMob(80, 160, 0, 60, 60)
+let zomb4 = new cMob(200, 350, 0, 60, 60)
+
+let door1 = new cDoor(650, 111, 0, 0 ,0)
+let door2 = new cDoor(650, 70, 0, 0 ,0)
+let door3 = new cDoor(650, 369, 0, 0 ,0)
+let door4 = new cDoor(650, 400, 0, 0 ,0)
 
 
 
- function playerZombieCol(player, zomb1, canvas)
+ function playerZombieCol(player, zomb1, canvas, life1, life2, life3)
  {
-   let playerPos = (player.x, player.y);
-   let ZombPos = (zomb1.x, zomb1.y);
-  // if(zomb2.x == zomb1.x)
-  //   {
-  //     console.log('YAY!!!');
-  //   }
-  if(playerPos == ZombPos)
-    {
-        playerPos = player.x - player.width;
-        console.log("You died!")
+   if(player.x == zomb1.x && player.y == zomb1.y )
+   {
 
-    }
-  }
+    player.x = zomb1.x - player.width;
+    //player.y = zomb1.y - player.height;
+
+    life1.x = -100;
+    life2.x = -100;
+    life3.x = -100;
+
+    console.log("You are Dead!")
+   }
+   else if(player.x == zomb2.x && player.y == zomb2.y )
+   {
+
+    player.x = zomb2.x - player.width;
+    //player.y = zomb1.y - player.height;
+
+    life1.x = -100;
+    life1.x = -100;
+    life1.x = -100;
+   }
+   else if(player.x == zomb3.x && player.y == zomb3.y )
+   {
+
+    player.x = zomb3.x - player.width;
+    //player.y = zomb1.y - player.height;
+
+    life1.x = -100;
+    life1.x = -100;
+    life1.x = -100;
+   }
+   else if(player.x == zomb4.x && player.y == zomb4.y )
+   {
+
+    player.x = zomb4.x - player.width;
+    //player.y = zomb1.y - player.height;
+
+    life1.x = -100;
+    life1.x = -100;
+    life1.x = -100;
+   }
+   if (life1.x ==-100 && life2.x == -100 && life3.x == -100)
+   {
+     alert("You are Dead!!!")
+   }
+
+ }
 
 
- function moveZomb(zomb1)
+class cLife implements iGameChar
 {
+  constructor(public x:number, public y:number, public speed:number, public width:number, public height: number){
+    x = 0;
+    y = 0;
+    speed = 0;
 
-   //zomb1.y += 5;
-  // zomb2.y += 3;
-  //  zomb3.y += 5;
-  //  zomb4.y += 7;
-  //  if(zomb1.y >= 589)
-  //  {
-  //    zomb1.y = -100;
-   //
-  //  }
-
-  //  if(zomb2.y >= 589)
-  //  {
-  //    zomb2.y = -100;
-  //  }
-  //  //
-  //  if(zomb3.y >= 589)
-  //  {
-  //    zomb3.y = -100;
-  //  }
-  //  //
-  //  if(zomb4.y >= 589)
-  //  {
-  //    zomb4.y = -100;
-  //  }
-
+  }
+  public draw = (): void =>
+  {
+    ctx.save();
+    ctx.translate(this.width, this.height);
+    ctx.translate(this.x, this.y);
+    ctx.drawImage(lifeImg, 0, 0, 30, 30);
+    ctx.restore();
+  }
 }
 
+let life1 = new cLife(0, -30, 0, 50, 50)
+let life2 = new cLife(30, -30, 0, 50, 50)
+let life3 = new cLife(60, -30, 0, 50, 50)
 
 
 
+     function moveZomb(zomb2,zomb3,zomb4)
+    {
 
+       zomb1.x -= 5;
+       zomb2.x -= 3;
+       zomb3.x -= 5;
+       zomb4.x -= 7;
+       if(zomb1.x < -100)
+       {
+         zomb1.x = 650;
+       }
+       //
+       if(zomb2.x < -100)
+       {
+         zomb2.x = 650;
+       }
+       //
+       if(zomb3.x < -100)
+       {
+         zomb3.x = 650;
+       }
+       //
+       if(zomb4.x < -100)
+       {
+         zomb4.x = 650;
+       }
+       //
 
- // function moveZomb(zomb1,canvas)
- //    {
- //      zomb1.x -=8;
- //      if(zomb1.x + zomb1.width > canvas.width)
- //      {
- //        zomb1.x = canvas.width - zomb1.width*3;
- //        zomb1.x =0;
- //        zomb1.x++
- //      }
- //      if(zomb1.y + zomb1.height > canvas.height)
- //      {
- //        zomb1.y = canvas.height - zomb1.height*3;
- //      }
- //      if(zomb1.x < -50)
- //      {
- //        zomb1.x =  -40;
- //      }
- //      if(zomb1.y < -50)
- //      {
- //        zomb1.y = -40;
- //      }
- //    }
-
-
-  //   function moveZomb(zomb1,zomb2,zomb3,zomb4)
-  //  {
-   //
-  //     zomb1.y += 5;
-  //     zomb2.y += 3;
-  //     zomb3.y += 5;
-  //     zomb4.y += 7;
-  //     if(zomb1.y >= 589)
-  //     {
-  //       zomb1.y = -100;
-  //     }
-  //     //
-  //     if(zomb2.y >= 589)
-  //     {
-  //       zomb2.y = -100;
-  //     }
-  //     //
-  //     if(zomb3.y >= 589)
-  //     {
-  //       zomb3.y = -100;
-  //     }
-  //     //
-  //     if(zomb4.y >= 589)
-  //     {
-  //       zomb4.y = -100;
-  //     }
-   //
-  //  }
+    }
